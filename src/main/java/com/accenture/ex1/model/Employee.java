c700e381e52597bc2a85dabcf2adab1967dcfd10
bkbatchelor <brian.batchelor@insidethesandbox.io>
2020-05-08 01:03:10 -0400

    - configured mongodb cloud - implemented post employee

--- src/main/java/com/accenture/ex1/model/Employee.java
+++ src/main/java/com/accenture/ex1/model/Employee.java
@@ -4,26 +4,30 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Employee{
+@Document(collection = "employee")
+public class Employee {
+
+	@Id
+	@JsonProperty("_id")
+	private String id;
+
+	@JsonProperty("position")
+	private String position;
+
+	@JsonProperty("contact")
+	private Contact contact;
 
 	@JsonProperty("address")
 	private Address address;
 
 	@JsonProperty("phone")
 	private Phone phone;
-
-	@JsonProperty("contact")
-	private Contact contact;
-
-	@JsonProperty("id")
-	private String id;
-
-	@JsonProperty("position")
-	private String position;
 
 	public Employee(String id) {
 		this.id = id;
