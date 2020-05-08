5cd635342df4c6688eb32bdadcd77e30cdd132ea
bkbatchelor <brian.batchelor@insidethesandbox.io>
2020-05-08 13:43:55 -0400

    Refactor

--- src/main/java/com/accenture/ex1/model/EmployeeWrapper.java
+++ src/main/java/com/accenture/ex1/model/EmployeeWrapper.java
@@ -3,19 +3,26 @@ package com.accenture.ex1.model;
 
 import lombok.Getter;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 public class EmployeeWrapper {
 
     private List<Employee> employeeList;
 
+    private Map<String, List<String>> employeeHash;
+
     public EmployeeWrapper(Employee employee) {
-        employeeList = Arrays.asList(employee);
+        employeeList = Collections.singletonList(employee);
     }
 
     public EmployeeWrapper(List<Employee> employeeList) {
         this.employeeList = employeeList;
+    }
+
+    public EmployeeWrapper(Map<String, List<String>> listMap) {
+        this.employeeHash = listMap;
     }
 }
