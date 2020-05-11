499fe0f733a835eba39cb597ad812709bf693b39
bkbatchelor <brian.batchelor@insidethesandbox.com>
2020-05-11 18:04:01 -0400

    Refactor to a multi-module maven project

--- src/main/java/com/insidethesandbox/spring/boot/cloud/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.insidethesandbox.spring.boot.cloud.repository;
-
-import com.insidethesandbox.spring.boot.cloud.model.employee.Employee;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends MongoRepository<Employee, String> {
-}
