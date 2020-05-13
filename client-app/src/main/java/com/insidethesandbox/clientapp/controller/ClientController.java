ca9bdadd39a5030064149eb965a576ffc57f033e
bkbatchelor <brian.batchelor@insidethesandbox.com>
2020-05-13 00:21:02 -0400

    Add get all employees to client app

--- client-app/src/main/java/com/insidethesandbox/clientapp/controller/ClientController.java
+++ client-app/src/main/java/com/insidethesandbox/clientapp/controller/ClientController.java
@@ -4,16 +4,13 @@ import com.insidethesandbox.clientapp.model.employee.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/client")
+@RequestMapping("/client-app")
 public class ClientController {
     private RestTemplate restTemplate;
 
@@ -27,11 +24,15 @@ public class ClientController {
     @PostMapping("/employee")
     public Employee.Wrapper createEmployee(@Valid @RequestBody Employee employee) {
         HttpEntity<Employee> request = new HttpEntity<>(employee);
-        return restTemplate.exchange("http://localhost:8081/employee", HttpMethod.POST, request, Employee.Wrapper.class).getBody();
+        return restTemplate.exchange("http://localhost:8081/employee-service/employee", HttpMethod.POST, request, Employee.Wrapper.class).getBody();
     }
 
 
     //get all employee
+    @GetMapping("/employees")
+    public Employee.Wrapper getEmployees() {
+        return restTemplate.getForObject("http://localhost:8081/employee-service/employees", Employee.Wrapper.class);
+    }
 
     //get employee by id
 
